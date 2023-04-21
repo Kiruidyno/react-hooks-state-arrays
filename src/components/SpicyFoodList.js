d9c9fb1034452036b5a368b6329070f471f64c5e
Kiruidyno <kirui472@gmail.com>
2023-04-22 01:57:13 +0300

    usestate update

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,40 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    console.log(newFoodArray);
   }
 
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    };
+
+    setFoods(newFoodArray);
+  }
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+  
+  return (
+    <select name="filter" onChange={handleFilterChange}>
+      <option value="All">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+    </select>
+  );
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key= {food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
